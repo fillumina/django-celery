2f4419ead3978b2efb0eb7c6ac79f534d730e450
Francesco Illuminati <fillumina@gmail.com>
2025-01-16 18:00:56 +0100

    add: print messages from web endpoints

--- project/tasks/views.py
+++ project/tasks/views.py
@@ -16,11 +16,13 @@ def run_task(request):
         task_type = request.POST.get("type")
         # the returned task is of type AsyncResult
         task = create_task.delay(int(task_type))
+        print(f'received request for task: {task.id}')
         return JsonResponse({"task_id": task.id}, status=202)
 
 
 @csrf_exempt
 def get_status(request, task_id):
+    print(f'requested status for task: {task_id}')
     # to retrieve a result from celery call AsyncResult(task_id)
     task_result = AsyncResult(task_id)
     result = {
