eafcecc695b619aed9b1b1d34d8aac4cde668fa6
Francesco Illuminati <fillumina@gmail.com>
2025-01-16 18:01:14 +0100

    add: logging to celery worker

--- project/core/celery.py
+++ project/core/celery.py
@@ -4,16 +4,21 @@ import os
 # with 'docker compose up -d --build'
 # or use 'pip install celery' from inside the container
 from celery import Celery
+from celery.utils.log import get_task_logger
 
 # celery needs to know where to find the django project
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", __name__ + ".settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 # create a new celery instance
 # the first argument is the name of the current module
 # app = Celery('tasks', backend='redis://localhost', broker='pyamqp://')
 # the other arguments can be defined in the settings
-app = Celery(__name__)
+app = Celery("core")
 # load default settings (option named CELERY_xxx)
 app.config_from_object("django.conf:settings", namespace="CELERY")
 # look for celery tasks (annotated with @shared_task)
 app.autodiscover_tasks()
+
+# define a logger
+logger = get_task_logger("core")
+
--- project/tasks/sample_tasks.py
+++ project/tasks/sample_tasks.py
@@ -1,14 +1,24 @@
 import time
 
 from celery import shared_task
+from core.celery import logger
 
-# celery task function (auto discovered by configuration in core/celery.py)
+# celery task function (auto discovered by configuration in core/celery.py and core/settings.py)
 # it will be executed by the celery worker not by the web application
 # - use create_task() to call this method from django web application (will return True)
 # - use create_task.delay() to call this method from Celery (will return an AsyncResult)
 #   (see views.py run_task(request))
 #   the actual result can be retrieved by invoking AsyncResult(task_id)
+#
+# there is another annotation available which is @app.task where app is the
+# one variable defined in `/core/celery.py`. By this way many Python process can use the
+# same worker each with its own settings. If different processes should share the same
+# task you have two annotation choices:
+# - @app.task(shared = True) use the settings provided by the `app` application
+# - @shared_task use the settings of the application that calls it
+# see: https://stackoverflow.com/questions/21233089/how-do-i-use-the-shared-task-decorator-for-class-based-tasks?answertab=votes#tab-top
 @shared_task
 def create_task(task_type):
+    logger.info('worker executing task')
     time.sleep(int(task_type) * 5)
     return 'this is the result'
