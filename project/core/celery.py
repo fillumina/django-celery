f7c1de1c8fe0a5a9eba5b6ca2f4c70adc32d974a
Francesco Illuminati <fillumina@gmail.com>
2025-01-16 17:56:22 +0100

    files: move pdfs in their own folder

--- project/core/celery.py
+++ project/core/celery.py
@@ -6,13 +6,13 @@ import os
 from celery import Celery
 
 # celery needs to know where to find the django project
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", __name__ + ".settings")
 
 # create a new celery instance
 # the first argument is the name of the current module
 # app = Celery('tasks', backend='redis://localhost', broker='pyamqp://')
 # the other arguments can be defined in the settings
-app = Celery("core")
+app = Celery(__name__)
 # load default settings (option named CELERY_xxx)
 app.config_from_object("django.conf:settings", namespace="CELERY")
 # look for celery tasks (annotated with @shared_task)
